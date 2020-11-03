9d85ee04a6223799b7a38b2f365002965d5b46ba
Luyangyang <1972887669@qq.com>
2020-11-03 18:19:19 +0800

    commit add Date.c

--- Date.c
+++ Date.c
@@ -1,27 +1,48 @@
-struct Date {
-  int year;
-  int month;
-  int day;
-};
-void next_day(struct Date day);
-void previous_day(struct Date day);
-int equal(struct Date one, struct Date day);
-void show(struct Date day) {
-  printf();
-}
-int main() {
-  struct Date today = {2018,3,20};
-  struct Date day = {2018,3,20};
-  show(day);
-  for(int i=0;i<30;i++)
-    next_day(day);
-  show(day);
-  for(int i=0;i<30;i++)
-    previous_day(day);
-  show(day);
-  if(equal(today, day))
-    printf("Very Good!\n");
-  else
-    printf("Very Bad!\n");
+/* Prints a date in legal form */
+#include <stdio.h>
+int main(void)
+{
+  int week,month, day, year;
+
+  printf("Enter date (ww/mm/dd/yy): ");
+  scanf("%d/%d/%d/%d",&week,&month,&day,&year);
+
+  printf("Dated this %d", day);
+  switch (day) {
+    case 1: case 21: case 31:
+      printf("st"); break;
+    case 2: case 22:
+      printf("nd"); break;
+    case 3: case 23:
+      printf("rd"); break;
+    default: printf("th"); break;
+  }
+  printf(" day of ");
+  switch (month) {
+    case 1:  printf("January");   break;
+    case 2:  printf("February");  break;
+    case 3:  printf("March");     break;
+    case 4:  printf("April");     break;
+    case 5:  printf("May");       break;
+    case 6:  printf("June");      break;
+    case 7:  printf("July");      break;
+    case 8:  printf("August");    break;
+    case 9:  printf("September"); break;
+    case 10: printf("October");   break;
+    case 11: printf("November");  break;
+    case 12: printf("December");  break;
+  }
+  printf(", 20%.2d,", year);
+  switch(week){
+  	case 1: printf("Monday");     break;
+  	case 2: printf("Tuesday");    break;
+  	case 3: printf("Wednesday");  break;
+  	case 4: printf("Thursday");   break;
+  	case 5: printf("Friday");     break;
+  	case 6: printf("Saturday");   break;
+  	case 7: printf("Sunday");     break;
+  }
   return 0;
 }
+
+
